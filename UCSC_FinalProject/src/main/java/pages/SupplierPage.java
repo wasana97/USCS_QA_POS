058d26b19a55ea273e33057f5b96c990ffbded12
Ranasingha <madushankapremakumara309@gmail.com>
2024-11-20 22:02:18 +0530

    customer update

--- UCSC_FinalProject/src/main/java/pages/SupplierPage.java
+++ UCSC_FinalProject/src/main/java/pages/SupplierPage.java
@@ -1,10 +1,12 @@
 package pages;
 
+
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 
-public class SupplierPage extends BasePage{
+
+public class SupplierPage extends BasePage {
     @FindBy(xpath = "//span[text()='Suppliers']")
     private WebElement suppliersTab;
 
@@ -14,22 +16,194 @@ public class SupplierPage extends BasePage{
     @FindBy(xpath = "//input[@name='company_name']")
     private WebElement companyName;
 
+    @FindBy(xpath = "//input[@name='first_name']")
+    private WebElement firstName;
+
+    @FindBy(xpath = "//input[@name='last_name']")
+    private WebElement lastName;
+
+    @FindBy(xpath = "//input[@name='email']")
+    private WebElement email;
+
+    @FindBy(xpath = "//input[@name='phone_number']")
+    private WebElement phone_number;
+
     @FindBy(xpath = "(//input[@id='submitf'])[1]")
     private WebElement saveBtn;
 
+    @FindBy(xpath = "//form[@id='supplier_form']/div/div[1]/h3")
+    private WebElement supplierInfo;
+
+    @FindBy(xpath = "//span[text()=' Choose file']")
+    private WebElement ChooseFile;
+
+    @FindBy(xpath = "//input[@id='image_id']")
+    private WebElement image_id;
+
+    // Path to the image to upload
+    String filePath = "C:\\Users\\nisha\\Downloads.avatar.png";
+
+    // Method to upload the file
+    public void uploadFile(String s) {
+        // Upload the file using the file input element
+        image_id.sendKeys(filePath);
+    }
+
+    @FindBy(xpath = "//input[@id='address_1']")
+    private WebElement address_1;
+
+    @FindBy(xpath = "//input[@id='address_2']")
+    private WebElement address_2;
+
+    @FindBy(xpath = "//input[@id='city']")
+    private WebElement city;
+
+    @FindBy(xpath = "//input[@id='state']")
+    private WebElement state;
+
+    @FindBy(xpath = "//input[@id='zip']")
+    private WebElement zip;
+
+    @FindBy(xpath = "//input[@id='country']")
+    private WebElement country;
+
+    @FindBy(xpath = "//textarea[@id='comments']")
+    private WebElement comments;
+
+    @FindBy(xpath = "//textarea[@id='internal_notes']")
+    private WebElement internal_notes;
+
+    @FindBy(xpath = "//input[@id='account_number']")
+    private WebElement account_number;
+
+    @FindBy(xpath = "//select[@id='section_names']")
+    private WebElement section_names;
+
+    String selectedTerm = "None";
+
+    public void selectTerm(String term) {
+        section_names.clear();  // Clear any previous input
+        section_names.sendKeys(term);  // Type the desired term
+        selectedTerm = term;  // Store the selected term
+    }
+
+
+    @FindBy(xpath = "//input[@id='files_1']")
+    private WebElement files_1;
+
+    String filePath1 = "C:\\Users\\nisha\\IdeaProjects\\Selenium\\USCS_QA_POS\\UCSC_FinalProject\\src\\main\\resources";
+
+
+    // Method to upload the file
+    public void uploadFile1(String s) {
+        // Upload the file using the file input element
+        files_1.sendKeys(filePath1);
+    }
+
+    public void clickOnEditButton(){
+        editButton.click();
+    }
+
+    @FindBy(xpath = "//*[@id=\"sortable_table\"]/tbody/tr[1]/td[2]/div/a")
+    private WebElement editButton;
+
+
+    @FindBy(xpath = "//*[@id=\"delete\"]")
+    private WebElement deleteButton;
+
+    public void clickOnDeleteButton(){
+        deleteButton.click();
+    }
+
+
+    @FindBy(xpath = "//span[text()=' Mailing Labels']")
+    private WebElement mailingLabels;
+
+    public void clickOnMailingLabelsButton(){
+        mailingLabels.click();
+
+    }
+
+
+    @FindBy(xpath = "//span[text()=' Clear Selection']")
+    private WebElement clearSelectionButton;
+
+    public void clickOnClearSelectionButton(){
+        clearSelectionButton.click();
+
+    }
+
+    private void fillField(@org.jetbrains.annotations.NotNull WebElement field, String value) {
+        field.clear();
+        field.sendKeys(value);
+    }
 
     public SupplierPage(WebDriver driver) {
         super(driver);
     }
 
-    public void visitSupplierPage(){
+    public void visitSupplierPage() {
         suppliersTab.click();
     }
 
-    public void addNewSupplier(){
-        newSupplier.click();
-        companyName.sendKeys("ABC");
+
+    @FindBy(xpath = "//*[@id=\"choose_location_modal\"]/div/div/div[2]") // Location modal option
+    private WebElement location;
+
+    @FindBy(xpath = "//*[@id=\"choose_location_modal\"]/div/div/div[2]/ul/li[1]/a") // Location modal option
+    private WebElement locationDefault;
+
+    public void ignoreLocation() {
+        click(location);
+    }
+
+    //--Fill supplier Form--
+    //--Method to Fill the Entire supplier Form--
+    public void fillSupplierForm(String companyName, String firstName, String lastName, String email, String phone_number, String filePath, String address_1, String address_2, String city, String state, String zip, String country, String comments, String internal_notes, String account_number  ) {
+        fillField(this.companyName, companyName);
+        fillField(this.firstName, firstName);
+        fillField(this.lastName, lastName);
+        fillField(this.email, email);
+        fillField(this.phone_number, phone_number);
+        image_id.sendKeys(filePath);
+        fillField(this.address_1, address_1);
+        fillField(this.address_2, address_2);
+        fillField(this.city, city);
+        fillField(this.state, state);
+        fillField(this.zip, zip);
+        fillField(this.country, country);
+        fillField(this.comments, comments);
+        fillField(this.internal_notes, internal_notes);
+        //fillField(this.account_number, account_number);
+        //files_1.sendKeys(filePath1);
+        //section_names.sendKeys(term);  // Type the desired term
+        //selectedTerm = term;  // Store the selected term
+
+    }
+
+    //--Submit the Form--
+    public void submitForm() {
         saveBtn.click();
     }
 
+
+    public void addNewSupplier(String companyName, String firstName, String lastName, String email, String phone_number, String filePath, String address_1, String address_2, String city, String state, String zip, String country, String comments, String internal_notes, String account_number) throws InterruptedException {
+
+        // Click the "Add New Supplier" button
+        click(newSupplier);
+
+        // Wait for the form to load
+        presenceOfElementLocated(supplierInfo);
+
+        // Fill out the form
+        fillSupplierForm(companyName, firstName,lastName, email, phone_number, filePath, address_1, address_2,city,  state,  zip,  country,  comments,  internal_notes,  account_number );
+
+
+        Thread.sleep(2000);
+        //image_id.sendKeys(filePath);
+
+
+        // Submit the form
+        submitForm();
+    }
 }
--- UCSC_FinalProject/src/test/java/tests/SupplierTest.java
+++ UCSC_FinalProject/src/test/java/tests/SupplierTest.java
@@ -1,13 +1,223 @@
 package tests;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.testng.annotations.Test;
-import pages.PageFactory;
+
+
+import java.time.Instant;
+import java.util.concurrent.TimeUnit;
+
+import static pages.PageFactory.supplierPage;
 
 public class SupplierTest extends BaseTest {
     @Test
     public void testSupplier() {
-        PageFactory.supplierPage.visitSupplierPage();
-      //  PageFactory.supplierPage.addNewSupplier();
+        WebElement locationDefault = driver.findElement(By.xpath("//*[@id=\"choose_location_modal\"]/div/div/div[2]/ul/li[1]/a"));
+        locationDefault.click();
+        supplierPage.ignoreLocation();
+        supplierPage.visitSupplierPage();
+        //  PageFactory.supplierPage.addNewSupplier();
     }
 
+    @Test
+    public void testSupplierPageFeature() throws InterruptedException {
+        supplierPage.visitSupplierPage();
+        supplierPage.addNewSupplier("ABCDE",
+                "Nishanya",
+                "Shelani",
+                "nishanyashelanii@gmail.com",
+                "0769119963",
+                "C:\\Users\\nisha\\IdeaProjects\\Selenium\\USCS_QA_POS\\UCSC_FinalProject\\src\\main\\resources\\avatar.png",
+                "No.621/5/2",
+                "St.Sebastian Road",
+                "Ragama",
+                "Western Province",
+                "10100",
+                "Sri Lanka",
+                "Comment1",
+                "Note1",
+                "1234567899"
+                //"C:\\Users\\nisha\\Downloads.file1.jpg"
+        );
+
+        Thread.sleep(2000);
+        System.out.println("New Supplier added successfully!");
+    }
+
+
+
+
+
+
+    //EDIT A RECORD// ***************************************************************************************
+
+
+    @Test
+    public void testEditForm() throws InterruptedException {
+        Thread.sleep(1000);
+
+        supplierPage.clickOnEditButton();
+
+        // Step 1: Locate the form fields and clear the existing values
+        WebElement companyName = driver.findElement(By.xpath("//*[@id=\"company_name\"]"));
+        WebElement firstName = driver.findElement(By.xpath("//*[@id=\"first_name\"]"));
+        WebElement lastName = driver.findElement(By.xpath("//*[@id=\"last_name\"]"));
+        WebElement email = driver.findElement(By.xpath("//*[@id=\"email\"]"));
+        WebElement phone_number = driver.findElement(By.xpath("//*[@id=\"phone_number\"]"));
+
+        companyName.clear();
+        firstName.clear();
+        lastName.clear();
+        email.clear();
+        phone_number.clear();
+
+        Thread.sleep(2000);
+
+        // Step 2: Enter new values
+        companyName.sendKeys("John's Company");
+        firstName.sendKeys("John");
+        lastName.sendKeys("Doe");
+        email.sendKeys("johndoe@gmail.com");
+        phone_number.sendKeys("076123456");
+
+        Thread.sleep(1000);
+
+        // Step 3: Submit the form
+        WebElement saveButton = driver.findElement(By.id("submitf"));
+        saveButton.click();
+
+        Thread.sleep(2000);
+        System.out.println("You have successfully updated the supplier");
+    }
+
+
+
+
+
+    //DELETE A RECORD// ****************************************************************************************
+
+    @Test
+    public void testDeleteForm() throws InterruptedException {
+        Thread.sleep(1000);
+
+        WebElement checkbox = driver.findElement(By.xpath("//*[@id=\"sortable_table\"]/tbody/tr[1]/td[1]/label/span"));
+        if (!checkbox.isSelected()) {
+            checkbox.click();
+
+            WebElement deleteButton = driver.findElement(By.id("delete"));
+            deleteButton.click();
+
+            Thread.sleep(2000);
+
+            WebElement cancelButton = driver.findElement(By.xpath("/html/body/div[6]/div/div/div[2]/button[1]"));
+            WebElement okButton = driver.findElement(By.xpath("/html/body/div[6]/div/div/div[2]/button[2]"));
+
+            if (cancelButton.isSelected()){
+                cancelButton.click();
+                Thread.sleep(1000);
+            }else{
+                okButton.click();
+                Thread.sleep(1000);
+                System.out.println("You have successfully deleted the supplier");
+            }
+        }
+    }
+
+
+
+
+
+
+//SEARCH A RECORD// ****************************************************************************************
+
+
+    @Test
+    public void testSearchFunctionality() throws InterruptedException {
+
+        // Step 1: Locate the search box and enter a query
+        WebElement searchBox = driver.findElement(By.id("search"));
+        WebElement searchButton = driver.findElement(By.xpath("//*[@id=\"search_form\"]/div/ul/li[2]/button"));
+        WebElement crossButton = driver.findElement(By.xpath("//*[@id=\"search_form\"]/div/ul/li[3]"));
+
+        searchBox.sendKeys("Shelani");
+        searchButton.click();
+        Thread.sleep(1000);
+        crossButton.click();
+        System.out.println("You have successfully searched the supplier");
+
+//        searchBox.clear();
+//        searchBox.sendKeys("45");
+
+        supplierPage.visitSupplierPage();
+    }
+
+
+
+
+//CLEAR SELECTION// ****************************************************************************************
+
+    @Test
+    public void clearSelection() throws InterruptedException {
+        Thread.sleep(1000);
+
+        WebElement checkbox = driver.findElement(By.xpath("//*[@id=\"sortable_table\"]/tbody/tr[1]/td[1]/label/span"));
+        if (!checkbox.isSelected()) {
+            checkbox.click();
+            Thread.sleep(1000);
+
+            WebElement clearSelectionButton = driver.findElement(By.xpath("//*[@id=\"content\"]/div[3]/div[1]/div[2]/div/a[4]"));
+            clearSelectionButton.click();
+
+            Thread.sleep(2000);
+            System.out.println("You have successfully cleared the selected one");
+
+        }
+    }
+
+
+//MAILING LABELS// ****************************************************************************************
+
+
+
+
+    @Test
+    public void mailingLabels() throws InterruptedException {
+        Thread.sleep(1000);
+
+        WebElement checkbox = driver.findElement(By.xpath("//*[@id=\"sortable_table\"]/tbody/tr[1]/td[1]/label/span"));
+        if (!checkbox.isSelected()) {
+            checkbox.click();
+            Thread.sleep(1000);
+
+            WebElement mailingLabelButton = driver.findElement(By.id("labels"));
+            mailingLabelButton.click();
+
+            Thread.sleep(2000);
+            System.out.println("You have successfully selected the Mailing Labels Button");
+        }
+    }
+
+
+
+    //EMAIL// ****************************************************************************************
+
+    @Test
+    public void sendEmail() throws InterruptedException {
+        Thread.sleep(1000);
+
+        WebElement checkbox = driver.findElement(By.xpath("//*[@id=\"sortable_table\"]/tbody/tr[1]/td[1]/label/span"));
+        if (!checkbox.isSelected()) {
+            checkbox.click();
+            Thread.sleep(1000);
+
+            WebElement clickEmailButton = driver.findElement(By.xpath("//*[@id=\"email\"]"));
+            clickEmailButton.click();
+
+            Thread.sleep(2000);
+            System.out.println("You have successfully selected");
+
+        }
+    }
 }
